fa658d07a90f7289057d30d9b14d4f301358b4f0
arell74 <farelfauzan36@gmail.com>
2025-09-26 21:29:50 +0700

    dasar tipe data

--- main.cpp
+++ main.cpp
@@ -3,7 +3,12 @@
 using namespace std;
 int main() {
     cout << "hello world" << endl;
-    cout << "hell yeah" << endl << endl << endl;
-    cout << "Farel Raiden";
+    int umur = 18;
+    const double tinggi = 189.5;
+
+    umur = 20;
+
+    cout << "my umur is " << umur << " dan " << "my tinggi is " << tinggi;
+
     return 0;
 }
