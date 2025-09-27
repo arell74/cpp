4a5e4fb82f233fec61cf58a53575af6be8245f1c
arell74 <farelfauzan36@gmail.com>
2025-09-27 22:21:46 +0700

    Tugas 2

--- main.cpp
+++ main.cpp
@@ -2,13 +2,25 @@
 
 using namespace std;
 int main() {
-    cout << "hello world" << endl;
-    int umur = 18;
-    const double tinggi = 189.5;
+    // cout << "hello world" << endl;
+    // int umur = 18;
+    // const double tinggi = 189.5;
 
-    umur = 20;
+    // umur = 20;
 
-    cout << "my umur is " << umur << " dan " << "my tinggi is " << tinggi;
+    // cout << "my umur is " << umur << " dan " << "my tinggi is " << tinggi;
+
+    // int x = 49;
+    // int y = 51;
+    // int z = x + y;
+
+    // cout << "Hasil dari x + y adalah " << z;
+
+    // tugas 2
+    int a = 5;
+    int b = 15;
+    int c = a + b;
+    cout << "Penjumlahan antara " << a << " dengan nilai " << b << " adalah " << c;
 
     return 0;
 }
