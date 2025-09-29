2f13d057aaba731831583498b52ccc2f1dd79c55
arell74 <farelfauzan36@gmail.com>
2025-09-29 22:27:58 +0700

    Latihan: Registration & Login (2)

--- login/main.cpp
+++ login/main.cpp
@@ -3,13 +3,14 @@
 using namespace std;
 
 class temp {
-    string userName,email,password;
+    string userName, email, password;
+    string searchName, searchPass, searchEmail;
     fstream file;
-    public:
+public:
     void login();
     void signUp();
     void forgot();
-}obj;
+} obj;
 
 int main() {
     char choice;
@@ -19,65 +20,84 @@ int main() {
     cout << "\n4- Exit";
     cout << "\nEnter Your Choice: ";
     cin >> choice;
+    cin.ignore();
 
     switch(choice) {
-        case '1':
-
-        break;
-        case '2':
-
-        break;
-        case '3':
-
-        break;
-        case '4':
-
-        break;
-        default:
-            cout << "Invalid Selection..";
+        case '1': obj.login(); break;
+        case '2': obj.signUp(); break;
+        case '3': obj.forgot(); break;
+        case '4': return 0;
+        default: cout << "Invalid Selection.." << endl;
     }
 }
 
-void temp :: signUp(){
-        cout << "\nEnter your username: ";
-        getline(cin, userName);
-        cout << "\nEnter your Email address: ";
-        getline(cin, email);
-        cout << "\nEnter your Password: ";
-        getline(cin, password);
+void temp::signUp() {
+    cout << "\nEnter your username: ";
+    getline(cin, userName);
+    cout << "Enter your Email address: ";
+    getline(cin, email);
+    cout << "Enter your Password: ";
+    getline(cin, password);
 
-        file.open("loginData.txt", ios :: out | ios :: app);
-        file<<userName<<"*"<<email<<"*"<<password<<endl;
-        file.close();
+    file.open("loginData.txt", ios::out | ios::app);
+    file << userName << "*" << email << "*" << password << endl;
+    file.close();
+
+    cout << "Sign Up berhasil!" << endl;
 }
 
-void temp :: login() {
-    string searchName, searchPass;
-    cout << "----- Login -----";
+void temp::login() {
+    cout << "----- Login -----" << endl;
     cout << "Masukkan Username anda: ";
-    getline(cin,searchName);
+    getline(cin, searchName);
     cout << "Masukkan Password anda: ";
-    getline(cin,searchPass);
+    getline(cin, searchPass);
 
-    file.open("loginData.txt", ios :: in);
-    getline(file,userName), '*';
-    getline(file,email), '*';
-    getline(file,password), '*';
-    getline(file,userName), '*';
-
-    while(!file.eof()){
-        if(userName == searchName);
-            if(password == searchPass) {
-                cout << "\nLogin Berhasil!";
-                cout << "\nUsername: " << userName<<endl;
-                cout << "\nemail: " << email<<endl;
-            } else {
-                cout << "Password salah coba lagi!";
-            }
+    file.open("loginData.txt", ios::in);
+    if (!file) {
+        cout << "Belum ada data tersimpan!" << endl;
+        return;
     }
 
-    getline(file,userName), '*';
-    getline(file,email), '*';
-    getline(file,password), '*';
-    getline(file,userName), '*';
+    bool found = false;
+    while (getline(file, userName, '*') &&
+           getline(file, email, '*') &&
+           getline(file, password)) {
+        if (userName == searchName && password == searchPass) {
+            cout << "\nLogin Berhasil!" << endl;
+            cout << "Username: " << userName << endl;
+            cout << "Email   : " << email << endl;
+            found = true;
+            break;
+        }
+    }
+    if (!found) cout << "Login gagal. Username atau Password salah." << endl;
+    file.close();
+}
+
+void temp::forgot() {
+    cout << "\nEnter your username: ";
+    getline(cin, searchName);
+    cout << "Enter your email address: ";
+    getline(cin, searchEmail);
+
+    file.open("loginData.txt", ios::in);
+    if (!file) {
+        cout << "Data tidak ditemukan!" << endl;
+        return;
+    }
+
+    bool found = false;
+    while (getline(file, userName, '*') &&
+           getline(file, email, '*') &&
+           getline(file, password)) {
+        if (userName == searchName && email == searchEmail) {
+            cout << "Akun Ketemu!" << endl;
+            cout << "Password kamu adalah: " << password << endl;
+            found = true;
+            break;
+        }
+    }
+    if (!found) cout << "Akun tidak ditemukan." << endl;
+    file.close();
 }
