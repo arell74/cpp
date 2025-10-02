96fbf84ef3dbbbe7aea4684155100a5fc3977fe6
arell74 <farelfauzan36@gmail.com>
2025-10-02 21:26:35 +0700

    learn Pointer (2) switch value

--- pointer/main.cpp
+++ pointer/main.cpp
@@ -2,16 +2,36 @@
 
 using namespace std;
 
+void tukar(int *a, int *b)
+{
+    int temp;
+    temp = *a;
+    *a = *b;
+    *b = temp;
+}
+
 int main()
 {
+    // int a = 10;
+    // int *b = &a;
+    // *b = 100;
+
+    // cout << "Mem adress of a : " << &a << endl;
+    // cout << "Nilai a Adalah : " << a << endl;
+
+    // // cout << "Mem adress of b : " << &b << endl;
+    // cout << "Nilai b Adalah : " << *b << endl;
+    // return 0;
+
     int a = 10;
-    int *b = &a;
-    *b = 100;
+    int b = 20;
+    
+    tukar(&a, &b);
 
     cout << "Mem adress of a : " << &a << endl;
     cout << "Nilai a Adalah : " << a << endl;
 
-    // cout << "Mem adress of b : " << &b << endl;
-    cout << "Nilai b Adalah : " << *b << endl;
+    cout << "Mem adress of b : " << &b << endl;
+    cout << "Nilai b Adalah : " << b << endl;
     return 0;
 }
